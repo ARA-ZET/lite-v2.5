75279df973399fd3fbccdba308e23c42082646c6
ARA-ZET <59029293+ARA-ZET@users.noreply.github.com>
2020-09-05 16:02:44 +0200

    Add files via upload

---
+++ lite/js/store.js
@@ -0,0 +1,232 @@
+const pantry = [{
+  id: 1,
+  category: "long life milk",
+  title: "PnP UHT Full Cream Milk 1l x 6",
+  price: 74.94,
+  img: "https: //cdn-prd-02.pnp.co.za/sys-master/images/h10/hd7/9888055525406/silo-product-image-v2-14Jul2020-180121-6001007164294-up-433526-50_400Wx400H",
+ },
+ {
+  id: 2,
+  category: "long life milk",
+  title: "PnP UHT Fat Free Milk 1l x 6",
+  price: 74.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h13/h99/9761394524190/silo-product-image-v2-07Apr2020-180122-6001007164317-up-418160-1199_140Wx140H",
+ },
+ {
+  id: 3,
+  category: "long life milk",
+  title: "PnP UHT Fat Free Milk 1l x 6",
+  price: 74.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h7b/h9a/9761392754718/silo-product-image-v2-07Apr2020-180122-6001007164300-up-418159-1198_140Wx140H",
+ },
+ {
+  id: 4,
+  category: "long life milk",
+  title: "clover UHT 2% milk 1lX6",
+  price: 95.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h60/hc8/9229788708894/silo-6001299015212-front-296887_140Wx140H",
+ },
+ {
+  id: 5,
+  category: "long life milk",
+  title: "clover UHT full cream milk 1lX6",
+  price: 95.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/hde/hb4/9506973155358/silo-product-image-v2-28May2019-180058-6001299015205-front-353954-611_140Wx140H",
+ },
+ {
+  id: 6,
+  category: "long life milk",
+  title: "Parmalat EverFresh UHT full Cream Milk 1lX6",
+  price: 98.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h81/h13/9243770355742/silo-6002100140598-front-306653_140Wx140H",
+ },
+ {
+  id: 7,
+  category: "long life milk",
+  title: "Earth & Co Rice Drink 1l",
+  price: 45.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h93/h70/9272316002334/silo-6009802364017-front-318979_140Wx140H",
+ },
+
+ {
+  id: 8,
+  category: "long life milk",
+  title: "Clover UHT full Cream milk 1l",
+  price: 16.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h8f/hdf/9229521649694/silo-6001299015274-front-296892_140Wx140H",
+ },
+ {
+  id: 9,
+  category: "long life milk",
+  title: "Almold Breeze MIlk Original 1l",
+  price: 39.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/root/h1c/haf/9146203045918/679917_EA_140Wx140H",
+ },
+
+ {
+  id: 10,
+  category: "long life milk",
+  title: "Good hope Chocolate Flavoured Milk 200ml",
+  price: 9.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h38/h9d/9295484878878/silo-6007359001614-front-321843_140Wx140H",
+ },
+
+
+ // SUGARS AND SWEATNERS
+
+
+ {
+  id: 11,
+  category: "sugars and sweatners",
+  title: "PnP sweatner Tablets 100ea",
+  price: 21.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h60/hcd/9671471562782/silo-product-image-v2-08Jan2020-180050-6001007234034-front-389410-1467_140Wx140H",
+ },
+
+ {
+  id: 12,
+  category: "sugars and sweatners",
+  title: "Canderel Sweatner Stick Sachet Green 80 each",
+  price: 88.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h26/hd4/9861501091870/silo-product-image-v2-26Jun2020-180106-7640110708255-front-431295-79_140Wx140H",
+ },
+
+ {
+  id: 13,
+  category: "sugars and sweatners",
+  title: "Canderel Yellow stick sachet 100ea",
+  price: 72.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h17/h37/9861484380190/silo-product-image-v2-26Jun2020-180111-7640110703847-front-431283-132_140Wx140H",
+ },
+
+ {
+  id: 14,
+  category: "sugars and sweatners",
+  title: "Canderel Sweater Tabs Yellow 17gX200",
+  price: 74.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h49/h2d/9861484642334/silo-product-image-v2-26Jun2020-180110-7640110703823-front-431297-119_140Wx140H",
+ },
+
+ {
+  id: 15,
+  category: "sugars and sweatners",
+  title: "Canderel Cafe Sticks 200X12",
+  price: 1174.49,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h21/h45/9152293961758/silo-19321778650319-front-107123_140Wx140H",
+ },
+
+ {
+  id: 16,
+  category: "sugars and sweatners",
+  title: "Hulets Sunsweat Brown sugar 3kg",
+  price: 55.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h4a/h64/9263537586206/silo-6009645681081-front-315088_140Wx140H",
+ },
+ {
+  id: 17,
+  category: "sugars and sweatners",
+  title: "Huletts white sugar 1kg",
+  price: 21.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/root/hbd/h80/9174356426782/258451_CK1_140Wx140H",
+ },
+ {
+  id: 18,
+  category: "sugars and sweatners",
+  title: "Hullets Sunsweat Brown sugar 1kg",
+  price: 17.44,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h96/h6a/9263555641374/silo-6009645681036-front-315087_140Wx140H",
+ },
+
+ {
+  id: 19,
+  category: "sugars and sweatners",
+  title: "Salti white Sugar 1kg",
+  price: 21.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h79/hb7/9844162527262/silo-product-image-v2-10Jun2020-180112-6001345000704-up-428743-200_140Wx140H",
+ },
+
+ {
+  id: 20,
+  category: "sugars and sweatners",
+  title: "Salati white Sugar 250g",
+  price: 5.99,
+  img: "https://cdn-prd-02.pnp.co.za/sys-master/images/h41/h40/9844163903518/silo-product-image-v2-10Jun2020-180113-6001345001800-up-428745-208_140Wx140H",
+ },
+
+]
+
+// LINKING WITH HTML
+const sectionCenter = document.querySelector(".products-center");
+const container = document.querySelector(".cat-btn");
+
+//load items
+window.addEventListener("DOMContentLoaded", function () {
+ displayMenuItems(pantry);
+ displayMenuButtons();
+});
+// FUNCTION THAT DISPLAY MENU ITEMS 
+function displayMenuItems(menuItems) {
+ let displayMenu = menuItems.map(function (item) {
+  //console.log(item)
+  return `<div class="single-product">
+    <div>
+     <img class="product"
+      src="${item.img}"
+      alt="${item.title}">
+    </div>
+    <div class="info">
+     <h2>R${item.price}</h2>
+     <img src="../images/icons/add-to-cart.png" alt="">
+    </div>
+    <div class="info">
+     <p>${item.title}</p>
+    </div>
+    <button class="add-to-cart">
+     <img src="../images/icons/cartplus.png" alt=""><span>
+      <p>ADD TO CART</p>
+     </span> </button>
+   </div>`;
+ });
+ displayMenu = displayMenu.join("");
+ // console.log(displayMenu);
+ sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
+ const categories = pantry.reduce(
+  function (values, item) {
+   if (!values.includes(item.category)) {
+    values.push(item.category);
+   }
+   return values
+  }, ['all']);
+ //console.log(categories)
+
+ const categoryBtns = categories.map(function (category) {
+   return `<button class="filter-btn" data-id="${category}">
+    ${category}
+    </button>`;
+  })
+  .join("");
+ container.innerHTML = categoryBtns;
+ const filterBtns = document.querySelectorAll(".filter-btn")
+
+ //filteritems
+
+ filterBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+   const category = e.currentTarget.dataset.id;
+   const menuCategory = pantry.filter(function (menuItem) {
+    if (menuItem.category === category) {
+     return menuItem
+    }
+   })
+   console.log(menuCategory);
+   if (category === "all") {
+    displayMenuItems(menu)
+   } else {
+    displayMenuItems(menuCategory)
+   }
+  });
+ });
+}
---
+++ lite/js/swiperjs.js
@@ -0,0 +1,22 @@
+let swiper = new Swiper(".swiper-container", {
+ effect: "coverflow",
+ grabCursor: true,
+ centeredSlides: true,
+ slidesPerView: "auto",
+ coverflowEffect: {
+  rotate: 120,
+  stretch: 0,
+  depth: 100,
+  modifier: 1,
+  slideShadows: true,
+ },
+ loop: true,
+ // autoplay: {
+ //  delay: 2500,
+ //  disableOnInteraction: true,
+ // },
+
+ pagination: {
+  el: ".swiper-pagination",
+ },
+});
